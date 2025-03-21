077a6672ba673f6f51dcef5600fa75ddcbd498ca
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-03-21 16:27:32 +1030

    truckloads.cpp

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -1,16 +1,11 @@
-#ifndef TRUCKLOADS_H
-#define TRUCKLAODS_H
+#include "Truckloads.h"
 
-class Truckloads {
-    public:
-        int numTrucks(int numCrates, int loadSize) {
-            if (loadSize >= numCrates) {
-                return 1;
-            } else {
-                int n1 = numCrates / 2;
-                int n2 = n1 + (numCrates % 2);
-                return numTrucks(n1, loadSize) + numTrucks(n2, loadSize);
-            }
-        }
-};
-#endif
+int Truckloads::numTrucks(int numCrates, int loadSize){
+    if (loadSize >= numCrates) {
+        return 1;
+    } else {
+        int n1 = numCrates / 2;
+        int n2 = n1 + (numCrates % 2);
+        return numTrucks(n1, loadSize) + numTrucks(n2, loadSize);
+    }
+}
---
+++ Truckloads.h
@@ -0,0 +1,8 @@
+#ifndef TRUCKLOADS_H
+#define TRUCKLAODS_H
+
+class Truckloads {
+    public:
+        int numTrucks(int numCrates, int loadSize);
+};
+#endif
