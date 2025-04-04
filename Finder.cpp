419764bbe309cd73734b55c5224c81dc0ce715d4
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-04-04 20:31:04 +1030

    as

--- Finder.cpp
+++ Finder.cpp
@@ -6,24 +6,30 @@
 
         vector<int> result;
 
-        for(size_t i = 1; i <= s2.size(); i++) {
-            size_t one = s1.find(s2[i]);
-            if (one == 0) {
-                return result;
-            }
-            size_t found = s1.find(s2.substr(0, i));
-            if (found != string::npos) {
-                result.push_back(found);
-            } else {
-                return result;
-            }
-        }
-        for (int i = 0; i < s2.size(); i++) {
-            result.push_back(-1);
-        }
-        for (int i = 1; i <= s1.size(); i++) {
-            for (int j = 0; j <= s2.size(); i++) {
-                if ()
+        // for(size_t i = 1; i <= s2.size(); i++) {
+        //     size_t one = s1.find(s2[i]);
+        //     if (one == 0) {
+        //         return result;
+        //     }
+        //     size_t found = s1.find(s2.substr(0, i));
+        //     if (found != string::npos) {
+        //         result.push_back(found);
+        //     } else {
+        //         return result;
+        //     }
+        // }
+        // for (int i = 0; i < s2.size(); i++) {
+        //     result.push_back(-1);
+        // }
+        for (int i = 0; i < s1.size(); i++) {
+            for (int j = 0; j < s2.size(); j++) {
+                if(s2[j] == s1[i+j]) {
+                    if (result[j] == -1) {
+                        result[j] = i;
+                    }
+                } else {
+                    break;
+                }
             }
         }
         return result;
