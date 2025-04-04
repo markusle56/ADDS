8055c3cb9a5290dad84e4294208eb3f4517b64d7
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-04-04 21:27:35 +1030

    final

--- Finder.cpp
+++ Finder.cpp
@@ -1,28 +1,15 @@
 #include "Finder.h"
-
-    using namespace std;
-
-    vector<int> Finder::findSubstrings(string s1, string s2) {
-
-        vector<int> result(s2.size(),-1);
-        size_t count = s1.find(s2.substr(0,1));
-        if (count == string::npos) {
-            return result;
+using namespace std;
+vector<int> Finder::findSubstrings(string s1, string s2) {
+    vector<int> result(s2.size(), -1);
+    size_t found = 0;
+    for(size_t i = 1; i <= s2.size(); i++) {
+        found = s1.find(s2.substr(0, i), found);
+        if (found != string::npos) {
+            result[i-1]=found;
         } else {
-            result[0] = count;
-        }
-        while (count != string::npos) {
-            for(int i = 1; i < s2.size(); i++) {
-                if (s2[i] == s1[i+count]) {
-                    if (result[i] != -1) {
-                        result[i] = count;
-                    }
-                }
-            }
-            count = s1.find(s2.substr(0,1), count);
-        }
-        return result;
-        
+            return result;
     }
-
-   
+    }
+    return result;
+}
