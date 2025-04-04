29978974c4ff82b97c01ebf31751bbe802c2e179
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-04-04 20:29:46 +1030

    as

--- Finder.cpp
+++ Finder.cpp
@@ -18,6 +18,14 @@
                 return result;
             }
         }
+        for (int i = 0; i < s2.size(); i++) {
+            result.push_back(-1);
+        }
+        for (int i = 1; i <= s1.size(); i++) {
+            for (int j = 0; j <= s2.size(); i++) {
+                if ()
+            }
+        }
         return result;
     }
 
