e4aad592cb0784fc45c961ea01a27c11a7067151
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-04-04 21:18:43 +1030

    as

--- Finder.cpp
+++ Finder.cpp
@@ -5,17 +5,21 @@
     vector<int> Finder::findSubstrings(string s1, string s2) {
 
         vector<int> result(s2.size(),-1);
-        size_t num = s1.find(s2.substr(0,1));
-        if (num == string::npos) {
+        size_t count = s1.find(s2.substr(0,1));
+        if (count == string::npos) {
             return result;
+        } else {
+            result[0] = count;
         }
-        for(size_t i = 1; i <= s2.size(); i++) {
-            size_t found = s1.find(s2.substr(0, i), num);
-            if (found != string::npos) {
-                result[i-1]=found;
-            } else {
-                break;
+        while (count != string::npos) {
+            for(int i = 1; i< s2.size(), i++) {
+                if (s2[i] == s1[i+count]) {
+                    if (result[i] != -1) {
+                        result[i] = count;
+                    }
+                }
             }
+            count = s1.find(s2.substr(0,1), count);
         }
         return result;
         
