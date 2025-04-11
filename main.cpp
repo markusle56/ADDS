a6a5d682c885edfbcbbe9fb75394242454a07289
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-04-11 19:56:54 +0930

    finbal

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
 #include "QuickSort.h"
 #include "RecursiveBinarySearch.h"
 
 int main() {
-    std::vector<int> nums = {1, 3, 5, 4, -5, 100, 7777, 2014};
+    std::string inputLine;
+    std::getline(std::cin, inputLine);
+    
+    std::istringstream iss(inputLine);
+    std::vector<int> nums;
+    int value;
+    while (iss >> value) {
+        nums.push_back(value);
+    }
+    // std::vector<int> nums = {1, 3, 5, 4, -5, 100, 7777, 2014, 0};
     QuickSort qs;
     std::vector<int> sortedList = qs.sort(nums);
     RecursiveBinarySearch rbs;
